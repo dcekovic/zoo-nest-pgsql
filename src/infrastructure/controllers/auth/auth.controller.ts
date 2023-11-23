71a440c8137d1f0770c37a73e2d4bdab91837bd5
dcekovic <72133046+dcekovic@users.noreply.github.com>
2023-11-23 13:26:53 +0100

    Update auth.controller.ts

--- src/infrastructure/controllers/auth/auth.controller.ts
+++ src/infrastructure/controllers/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { LoginUseCase } from 'src/usecases/auth/login.usecase';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('/auth')
+export class AuthController {
+  constructor(private readonly loginUseCase: LoginUseCase) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.loginUseCase.login(signInDto.username, signInDto.password);
+  }
+}
